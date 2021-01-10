66707b43be1d2e56423675adb394e4840bb42200
PeYouRoti <coyback22@gmail.com>
2021-01-10 19:37:04 +0100

    gucci

---
+++ blockchain.py
@@ -0,0 +1,460 @@
+import SHA_1
+from datetime import datetime
+import random
+from Clés import *
+import Clés
+import Signature_ElGamal
+#from test.test_buffer import verify_structure
+
+prime, alpha = 9897945769718193639959984638000900286314127960826347487029072470408703069670883110279720785601634769147223130343953316018693873378437092702305231198650081, 7867969773067674666283917880578211989084312205422864583887419974689917662993740993709666012206421341523769293969182149676889796906645634003337407639637527
+
+
+#Classe pour la création des utilisateurs (user : nom, argent, clé secrète, clé publique)
+class User(object):
+    """personne tenant un portefeuille"""
+    def __init__(self, prime, alpha):
+        super(User, self).__init__()
+        self.nom = input("Entrer votre nom d'utilisateur : ")
+        while self.nom.isdigit() or len(self.nom ) < 3 or len(self.nom ) > 15:
+            self.nom = input("Veuillez svp entrer un nom d'utilisateur valide : ")        
+        self.money = input("Entrer l'argent que vous détenez : ")
+        while not (self.money.isdigit() and (int(self.money) > 0)):
+            self.money = input("Entrer un montant valide : ")
+        self.money = int(self.money)
+        clés = publickey(prime, alpha)
+        self.public_key = clés[0]
+        self.secret = clés[1]    
+
+#Classe d'un Block (block : index, previous_hash, timestamp, salt, count, transactions)
+class Block(object):
+    def __init__(self, index, previousHash, timestamp, transactions):
+        self.index = index
+        self.previousHash = previousHash
+        self.timestamp = timestamp
+        self.salt = 0
+        #count : nombre de tentatives avant le minage du bloc
+        self.count=0
+        self.confirmedtransactions = transactions
+        self.hash = calculateHash(self)    
+    
+    #Fonction de minage d'un block
+    #Difficulty : définit le nombre de bits à 0 à la fin du hash pour le minage du bloc
+    def mineBlock(self, difficulty):
+        s1 = str(self.hash)
+        zeros = str("0"*difficulty)
+        self.salt = 0
+        self.count = 0
+        while not s1.endswith(zeros):
+            self.count += 1
+            self.salt = random.getrandbits(100)
+            self.hash = calculateHash(self)
+            s1 = str(self.hash)
+            
+#Calcul du hash du bloc entier (index + previoushash + timestamp + salt + count + transactions)
+def calculateHash(block):
+    bloc = str(block.index) + str(block.previousHash) + str(block.timestamp) + str(block.salt) +  str(block.count) + str(block.confirmedtransactions)
+    return (SHA_1.SHA_1(bloc))
+
+#Classe de la Blockchain
+class Blockchain(object):
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        #chain : liste des blocks de la blockchain
+        self.chain = []
+        #pending_transactions: transactions en attente
+        self.pending_transactions = []
+        self.list_users = []
+        self.all_transactions = []
+        #blocks : liste des blocks avant ajout à la blockchain
+        self.blocks = []
+        self.sign1 = []
+        self.sign2 = []
+        
+        #Génération du premier block de la chaîne
+        genesisBlock = Block(0, None, datetime.now(), None)
+        genesisBlock.mineBlock(self.difficulty)
+        self.chain.append(genesisBlock)
+    
+    #Création des utilisateurs    
+    def create_users(self, prime, alpha):
+        nombre_user = input("Combien d'utilisateur voulez-vous créer ? : ")
+        print ("Exemple avec les utilisateurs Benoit et Pierre\n")
+        while not (nombre_user.isdigit() and (int(nombre_user) > 0)):
+            nombre_user = input("Veuillez entrer un nombre d'utilisateurs correct : ")  
+        user_count = int(nombre_user)
+        i = 1
+        while not user_count == 0 :
+            user = User(prime, alpha)
+            self.list_users.append(user)
+            print("Utilisateur " + str(i) + " / " + str(nombre_user) + " créé.\n")
+            user_count -= 1
+            i += 1
+        self.display_users()
+        
+#----------------------------------------
+#---------------------------------------- Création et ajout des blocks à la blockchain
+#---------------------------------------- Création d'un bloc --> ajout des transactions (2 max par block) --> : minage du bloc --> ajout du block à la chaîne : ...   
+#----------------------------------------
+    
+    #Renvoie le block précédent
+    @property
+    def last_block(self):
+        return self.chain[-1]
+    
+    #Création d'un nouveau block avec les paramètres du block précédent
+    def newBlock(self):    
+        latestBlock = self.chain[-1]
+        new_block = Block(latestBlock.index + 1, latestBlock.hash, datetime.now(), self.pending_transactions)
+        self.blocks.append(new_block)
+        return new_block
+
+    #Minage puis vérification du block avant l'ajout à la chaîne
+    def addBlock(self, block):
+        block.mineBlock(self.difficulty)
+        if self.isValidBlock(block, self.last_block):
+            self.chain.append(block)
+            #Réinitialisation de la liste des transactions en attente quand création d'un nouveau block
+            self.pending_transactions = []
+        else:
+            print("block non valide pour être ajouté à la chaîne")
+
+
+#----------------------------------------
+#---------------------------------------- Transactions et signatures des transactions
+#----------------------------------------
+            
+    #Vérification de la signature d'une transaction       
+    def verify_transaction_signature(self, source, transaction):
+        source_secret = 0
+        source_public_key = 0
+        for user in self.list_users:
+            if user.nom == source:
+                source_secret = user.secret
+                source_public_key = user.public_key
+                break     
+        h = SHA_1.SHA_1(str(transaction))
+        signature = Signature_ElGamal.sign(h, alpha, prime, source_secret)
+        s1 = signature[0]
+        s2 = signature[1]
+        if s1 not in self.sign1:
+            self.sign1.append(s1)
+        if s2 not in self.sign2:
+            self.sign2.append(s2)
+        if not Signature_ElGamal.verif(h, alpha, prime, s1, s2, source_public_key):
+            print("Fail vérif signature")
+            return False
+        return True
+    
+    #Création d'une transaction avec vérification de sa signature avant sa validation
+    # + vérification des paramètres de la transaction (source, destination, amount)
+    def new_transaction(self, source, destination, amount):
+        amount = int(amount)
+        transaction = {
+            'sender': source,
+            'receiver' : destination,
+            'amount (BTC)' : amount,
+            } 
+        if self.verif_amount(source, destination, amount):
+            if self.verify_transaction_signature(source, transaction):
+                self.pending_transactions.append(transaction)
+                self.all_transactions.append(transaction)
+                for user_desti in self.list_users:
+                    if user_desti.nom == destination:
+                        user_desti.money += amount
+                for user_src in self.list_users:
+                    if user_src.nom == source:
+                        user_src.money -= int(amount)
+        else:
+            print("Echec de l'ajout de la transaction")
+        
+    #Vérification du montant de la transaction et du destinataire     
+    def verif_amount(self, source, destination, amount):
+        receiver_name = 0
+        amount = int(amount)
+        for user_dest in self.list_users:
+            if user_dest.nom == destination:
+                receiver_name = user_dest.nom
+        for user_source in self.list_users:
+            if user_source.nom == source:
+                if user_source.money < amount :
+                    print("Vous n'avez pas assez d'argent pour effectuer cette transaction")
+                    return False
+                if user_source.nom == receiver_name :
+                    print("Vous ne pouvez pas vous envoyer de l'argent") 
+                    return False    
+        return True    
+    
+    #Ajout d'une transaction par un utilisateur 
+    def add_transaction(self):
+        source = input("Qui envoie de l'argent : ")
+        destination = input("A quel destinaire voulez-vous envoyer de l'argent : ")
+        amount = input ("Combien voulez-vous envoyer ? : ")
+        transaction = {
+            'sender': source,
+            'receiver' : destination,
+            'amount (BTC)' : amount,
+            }
+        #Max 2 transactions par block
+        if self.verify_transaction_signature(source, transaction):
+            if len(self.pending_transactions) < 1:
+                if self.verif_amount(source, destination, amount):
+                    self.new_transaction(source, destination, amount)
+                    print("Nouvelle transaction ajoutée avec succès")
+            elif len(self.pending_transactions) == 1:
+                if self.verif_amount(source, destination, amount):
+                    self.new_transaction(source, destination, amount)
+                    block = self.newBlock()
+                    self.addBlock(block)
+                    print("Nouvelle transaction ajoutée avec succès")           
+                    print("Minage du bloc réussie")
+                    print("Ajout du bloc à la chaîne")
+            
+            for i in range(len(self.pending_transactions)):            
+                trans = ("\n\tTransaction en attente  :" + (str(self.pending_transactions[i]))+" --- Signature vérifiée")
+                print(trans)
+        else:
+            print("Vérification de la signature failed pour la nouvelle transaction")
+    
+    #Vérification de la signature d'une transaction par un utilisateur        
+    def verify_user_transaction(self):
+        print(str(len(self.all_transactions)) + " transactions au total")
+        index = int(input("Quel transaction voulez-vous vérifier ? : "))
+        transaction = self.all_transactions[index-1]
+        h = SHA_1.SHA_1(str(transaction))
+        print("Transaction " + str(index) + " : " + str(transaction))
+        source = input("Qui est à l'origine de la transaction ? : ")
+        for user in self.list_users:
+            if user.nom == source:
+                source_public_key = user.public_key
+                break
+        s1 = self.sign1[index-1]
+        s2 = self.sign2[index-1]   
+        if not Signature_ElGamal.verif(h, alpha, prime, s1, s2, source_public_key):
+            print("Cette signature n'a pas pu être vérifiée, veuillez contacter Rémi Cogranne")
+            return False
+        else:
+            print("Transaction " + str(index) + " : Signature vérifiée")
+            print(source, "est bien à l'origine de cette transaction")
+            return True  
+
+#----------------------------------------
+#---------------------------------------- Vérification de la blockchain
+#----------------------------------------
+    
+    #Vérification validité du premier bloc             
+    def isFirstBlockValid(self):
+        firstBlock = self.chain[0]
+        
+        #Vérification de l'index
+        if firstBlock.index != 0:
+            print("Erreur : index du premier bloc incorrect")
+            return False
+        
+        #Vérification du previous_hash
+        if firstBlock.previousHash is not None:
+            print("Erreur : 'previous_hash' du premier bloc incorrect - Should be 'None'")
+            return False
+        
+        #Vérification du hash
+        if (firstBlock.hash is None or calculateHash(firstBlock) != firstBlock.hash):
+            print("hash calculé :", calculateHash(firstBlock))
+            print("hash du bloc :", firstBlock.hash)
+            print("Erreur : hash du premier bloc incorrect")         
+            return False
+        return True
+    
+    
+    #Vérification validité d'un bloc                
+    def isValidBlock(self, block, previousBlock):
+        ind = block.index
+        
+        #Vérification de l'index
+        if previousBlock.index+1 != block.index:
+            print("Erreur : index du bloc " +str(ind)+ " incorrect")
+            return False
+        
+        #Vérification du previous_hash
+        if (block.previousHash is None or block.previousHash != previousBlock.hash):
+            print("Erreur : hash du bloc " + str(ind-1) +" différent du hash du bloc "+str(ind))
+            return False
+        
+        #Vérification du hash
+        if (block.hash is None or calculateHash(block) != block.hash):
+            print("Erreur : hash du bloc " + str(ind) +" différent du hash calculé")
+            return False
+        
+        #Vérification de la preuve de calcul : recalculer le hash et voir s'il correspond à la difficulté définie pour le minage
+        if not (str(calculateHash(block)).endswith(str("0"*self.difficulty))):
+            print("Erreur : preuve non valide pour le block " + str(ind))
+            return False      
+            
+        return True   
+    
+    #Vérification de la chaîne entière
+    def isBlockchainValid(self):
+        
+        #Vérification premier bloc
+        if not self.isFirstBlockValid():
+            return False
+        else:
+            print("Vérification du premier bloc réussie !")
+                
+        #Vérification des blocs suivants
+        m=0
+        for i in range(1, len(self.chain)):
+            previousBlock = self.chain[i-1]
+            block = self.chain[i]
+            if not self.isValidBlock(block, previousBlock):
+                m+=1
+                return False 
+        if m == 0:
+            print("Vérification des blocs suivants réussie !")                     
+        return True
+        
+
+#----------------------------------------
+#---------------------------------------- Affichage
+#---------------------------------------- 
+       
+    #Affichage de la blockchain
+    def display(self):
+        j=1
+        print("")
+        for block in self.chain:           
+            chain = "Block #"+str(block.index)+" ["+"\n\tindex: "+str(block.index)+"\n\tprevious hash: "+str(block.previousHash)+"\n\ttimestamp: "+str(block.timestamp)
+            print(str(chain))
+            if block.index == 0:
+                trans = ("\tTransaction : None")
+                print(trans)
+            else:
+                for i in range(len(block.confirmedtransactions)):            
+                    trans = ("\tTransaction " + str(j) + ": " + (str(block.confirmedtransactions[i]))+" --- Signature vérifiée")
+                    print(trans)
+                    j+=1               
+            chain2 = "\thash: "+str(block.hash)+"\n\tsalt: "+str(block.salt)+"\n\tcount: "+str(block.count)+"\n]\n"
+            print(str(chain2)+"\n")
+            
+        for i in range(len(self.pending_transactions)):            
+            trans = ("\tTransactions en attente  :" + (str(self.pending_transactions[i]))+" --- Signature vérifiée")
+            print(trans)
+            
+    #Affichage des utilisateurs
+    def display_users(self):
+        i = 1
+        for user in self.list_users:
+            chain = "\tUser " + str(i) + " : " + str(user.nom) + " | Argent disponible : " + str(user.money)
+            i += 1
+            print(str(chain)+"\n")
+
+    #Affichage du menu
+    def menu(self):
+        print("\nQue voulez-vous faire ? : ")
+        print("\t1 : Ajouter une nouvelle transaction à la chaîne")
+        print("\t2 : Vérifier la signature d'une transaction")
+        print("\t3 : Vérifier la validité de la blockchain")
+        print("\t4 : Afficher les comptes des utilisateurs")
+        print("\t5 : Ajouter un utilisateur")
+        print("\t6 : Afficher la blockchain")
+        print("\t7 : Exit")
+        rep = int(input())
+        return rep
+    
+    #Intéractions possibles via le menu
+    def play(self):
+        turnover=0
+        while turnover == 0:
+            rep = self.menu()
+            if rep == 1:
+                self.add_transaction()
+            if rep == 2:
+                self.verify_user_transaction()
+            if rep == 3:
+                print("Validité de la blockchain :", self.isBlockchainValid())
+            if rep == 4:
+                self.display_users()
+            if rep ==5:
+                self.create_users(prime, alpha)
+            if rep == 6:
+                self.display()
+            if rep == 7:
+                print("Fin d'exécution")
+                turnover += 1
+                
+def main(prime, alpha):
+    #----------------------------------------------------------------------------------------
+    #---------------------------------------- Main() ----------------------------------------
+    #----------------------------------------------------------------------------------------
+
+    #Générer un nombre premier 'prime' de 512 bits
+    #prime = gen_premier()
+        
+    #Trouver élement générateur 'alpha' de Zprime
+    #alpha = trouve_Zn(prime)
+
+    #Création de la blockchain (10 blocks)
+    blockchain = Blockchain(2)
+    blockchain.create_users(prime, alpha)
+
+    #Block 1
+    b1 = blockchain.newBlock()
+    t1 = blockchain.new_transaction("Pierre", "Benoit", "50")
+    t2 = blockchain.new_transaction("Benoit", "Pierre", "88")
+    blockchain.addBlock(b1)
+
+    #Block 2
+    b2 = blockchain.newBlock()
+    t3 = blockchain.new_transaction("Benoit", "Pierre", "300")
+    t4 = blockchain.new_transaction("Benoit", "Pierre", "4")
+    blockchain.addBlock(b2)
+
+    #Block 3
+    b3 = blockchain.newBlock()
+    t5 = blockchain.new_transaction("Pierre", "Benoit", "127")
+    t6 = blockchain.new_transaction("Pierre", "Benoit", "256")
+    blockchain.addBlock(b3)
+
+    #Block 4
+    b4 = blockchain.newBlock()
+    t7 = blockchain.new_transaction("Pierre", "Benoit", "91")
+    t8 = blockchain.new_transaction("Benoit", "Pierre", "79")
+    blockchain.addBlock(b4)
+
+    #Block 5
+    b5 = blockchain.newBlock()
+    t9 = blockchain.new_transaction("Pierre", "Benoit", "182")
+    t10 = blockchain.new_transaction("Benoit", "Pierre", "232")
+    blockchain.addBlock(b5)
+
+    #Block 6
+    b6 = blockchain.newBlock()
+    t11 = blockchain.new_transaction("Pierre", "Benoit", "225")
+    t12 = blockchain.new_transaction("Pierre", "Benoit", "241")
+    blockchain.addBlock(b6)
+
+    #Block 7
+    b7 = blockchain.newBlock()
+    t13 = blockchain.new_transaction("Benoit", "Pierre", "151")
+    t14 = blockchain.new_transaction("Pierre", "Benoit", "293")
+    blockchain.addBlock(b7)
+
+    #Block 8
+    b8 = blockchain.newBlock()
+    t15 = blockchain.new_transaction("Benoit", "Pierre", "256")
+    t16 = blockchain.new_transaction("Benoit", "Pierre", "130")
+    blockchain.addBlock(b8)
+
+    #Block 9
+    b9 = blockchain.newBlock()
+    t17 = blockchain.new_transaction("Pierre", "Benoit", "280")
+    t18 = blockchain.new_transaction("Benoit", "Pierre", "18")
+    blockchain.addBlock(b9)
+
+    #Block 10
+    b10 = blockchain.newBlock()
+    t19 = blockchain.new_transaction("Benoit", "Pierre", "79")
+    t20 = blockchain.new_transaction("Pierre", "Benoit", "251")
+    blockchain.addBlock(b10)
+
+    #Affichage blockchain et menu
+    blockchain.display()
+    blockchain.play()
---
+++ menu.py
@@ -0,0 +1,207 @@
+"""
+interface utilisateur
+"""
+from chiffrement import *
+from dechiffrement import *
+from gen_cle import gen_premier, trouve_Zn, publickey, privatekey
+from SHA_1 import SHA_1
+from fonction_eponge import eponge
+import Signature_ElGamal
+import RSA
+import pickle
+import blockchain
+from pip._vendor.pyparsing import originalTextFor
+
+#prime, alpha = gen_prime_et_alpha()
+prime, alpha = 9897945769718193639959984638000900286314127960826347487029072470408703069670883110279720785601634769147223130343953316018693873378437092702305231198650081, 7867969773067674666283917880578211989084312205422864583887419974689917662993740993709666012206421341523769293969182149676889796906645634003337407639637527
+# AlicePublicKey , AlicePrivateKey = gen_couple_cles_menu(prime, alpha)
+# print("Un couple de clés vous a été généré en prévision de vos opérations")
+
+PROMPT = ">>> "
+
+def main_menu_sequence():
+	print("CHARGEMENT ...")
+	print("-----------------*")
+
+	while True:
+		MENU_SEQUENCE = "Bonjour ô maître Rémi ! Que souhaitez vous faire aujourd’hui ?\n ->1<- Chiffrer un message.\n ->2<- Déchiffrer un message.\n ->3<- Générer des couples de clés publiques / privées.\n ->4<- Générer un hash / générer et vérifier une empreinte.\n ->5<- Enter dans le menu de gestion de la blockchain"
+		print(MENU_SEQUENCE)
+		arg = input(PROMPT)
+		if arg == '1': 
+			kasumi_encrypt_menu()
+		elif arg == '2':
+			kasumi_decrypt_menu()
+		elif arg == '3':
+			gen_couple_cles_menu(prime, alpha)
+		elif arg == '4':
+			print("Voulez vous générer : \n->1<- Un hash \n->2<- une empreinte") 
+			arg = input(PROMPT)
+
+			if arg == '1':
+				hash_menu()
+			elif arg == '2':
+				signature(prime, alpha)
+
+		elif arg == '5':
+			blockchain.main(prime, alpha)
+		# elif arg == '6':
+		# 	pass
+		# elif arg == '7':
+		# 	pass
+		# elif arg == '8':
+		# 	pass
+		# elif arg == '9':
+		# 	pass
+
+def kasumi_encrypt_menu():
+	print("Entrez ici votre clé de chiffrement (ATTENTION, ELLE DOIT ETRE AU MINIMUM DE 16 CARACTERES:") 
+	key = input(PROMPT)
+
+	# KASUMI ENCRYPT
+	print("Voulez vous \n->1<- écrire le texte à chiffrer ici \n->2<- Chiffrer le texte dans le fichier \"texte_a_chiffrer\"")
+	arg = input(PROMPT)
+
+	if arg == '1':
+		print("Quel texte voulez vous chiffrer :")
+		content = input(PROMPT)
+	elif arg == '2':
+		f=open("texte_a_chiffrer.txt", "r")
+		if f.mode == 'r':
+			content =f.read()
+			print("Le texte à chiffrer est :\n", content)
+	
+	print('Choisissez un type d\'algorithme. \n->1<- ECB \n->2<- CBC \n->3<- PCBC')
+	arg = input(PROMPT)
+	if arg == '1':
+		encrypted_content = kasumi_encrypt_ECB(content, key)
+	elif arg == '2':
+		encrypted_content = kasumi_encrypt_CBC(content, key)
+	elif arg == '3':
+		encrypted_content = kasumi_encrypt_PCBC(content, key)
+
+	print("Resultat du chiffrement : ", encrypted_content)
+
+	with open('texte_chiffré.txt', 'wb') as f:
+		pickle.dump(encrypted_content, f)
+
+	print("Votre chiffré se retrouve dans le fichier \"texte_chiffré\"")
+
+def kasumi_decrypt_menu():
+	print("Entrez ici votre clé de déchiffrement :") 
+	key = input(PROMPT)
+
+	print("ATTENTION, le texte à déchiffrer DOIT se trouver dans le fichier \"texte_chiffré.txt\"")
+	
+	with open("texte_chiffré.txt", "rb") as f:
+		encrypted_content = pickle.load(f)
+
+	print("Le texte à déchiffrer est :\n", encrypted_content)
+	
+	print('Choisissez un type d\'algorithme. \n->1<- ECB \n->2<- CBC \n->3<- PCBC')
+	arg = input(PROMPT)
+	if arg == '1':
+		decrypted_content = kasumi_decrypt_ECB(encrypted_content, key)
+	elif arg == '2':
+		decrypted_content = kasumi_decrypt_CBC(encrypted_content, key)
+	elif arg == '3':
+		decrypted_content = kasumi_decrypt_PCBC(encrypted_content, key)
+	
+	print("Resultat du déchiffrement : ", decrypted_content)
+
+def gen_prime_et_alpha():
+	prime=gen_premier()
+	print("Le nombre n suivant est PREMIER :", prime)
+	print("")
+
+	#Trouver élément générateur de Zprime
+	alpha=trouve_Zn(prime)
+	print("Un élement générateur de Zn :", alpha)
+	print("")
+	return prime, alpha
+
+def gen_couple_cles_menu(prime, alpha):
+
+	#Génération d'un couple de clés pour Alice et Bob
+	clés_alice = publickey(prime, alpha)
+	AlicePublicKey = clés_alice[0]
+	AliceSecret = clés_alice[1]
+	AlicePrivateKey = privatekey(AlicePublicKey, prime, AliceSecret)
+
+	print( "GENERATION DE CLE\n-------------------------*\nCLE PUBLIC : \n", AlicePublicKey,"\n-------------------------*\nCLE PRIVEE : \n", AlicePrivateKey)
+	print("\n\nCes clés ont été sauvegardées dans \"generated_public_key\" et \"generated_private_key\"\n")
+	with open('generated_public_key.txt','wb') as fichier:
+		mon_pickler=pickle.Pickler(fichier)
+		mon_pickler.dump(AlicePublicKey)
+	
+	#stockage secrets
+	with open('generated_private_key.txt','wb') as fichier2:
+		pickler=pickle.Pickler(fichier2)
+		pickler.dump(AliceSecret)
+	return AlicePublicKey , AlicePrivateKey, AliceSecret
+
+def hash_menu():
+	print("Voulez vous utiliser la fonction éponge ? \n->1<- Non \n->2<- Oui")
+	arg = input(PROMPT)
+	print("Entrez ici le texte à hasher") 
+	texte = input(PROMPT)
+
+	if arg == '1': 
+		print("Resultat du hash :\n", SHA_1(texte),"\n")
+	elif arg == '2':
+		print("Resultat du hash :\n",eponge(texte),"\n")
+
+def signature(prime, alpha):
+	print("Entrez le texte à signer") 
+	texte = input(PROMPT)
+	print("Quel type de signature ? \n->1<- El-Gamal \n->2<- RSA") 
+	arg = input(PROMPT)
+	
+	if arg == '1': 
+		print("Qui est à l'origine du message ? \n->1<- Alice \n->2<- Bob")
+		source = int(input(PROMPT))
+		texte_hashé = SHA_1(texte)
+		verif = False
+		origin = "none"
+		s1 = 0
+		s2 = 0
+		AlicePublicKey , AlicePrivateKey, AliceSecret = gen_couple_cles_menu(prime, alpha)
+		BobPublicKey , BobPrivateKey, BobSecret = gen_couple_cles_menu(prime, alpha)
+		if source == 1 :
+			s1, s2 = Signature_ElGamal.sign(texte_hashé, alpha, prime, AliceSecret)
+		elif source == 2 :
+			s1, s2 = Signature_ElGamal.sign(texte_hashé, alpha, prime, BobSecret)
+		print("La signature est la suivante :", s1, s2)	
+		print("Vérifier qu'Alice ou Bob est bien à la source du message ? \n->1<- Alice \n->2<- Bob")
+		arg = input(PROMPT)
+		if arg == '1': 
+			verif = Signature_ElGamal.verif(SHA_1(texte), alpha, prime, s1, s2 , AlicePublicKey)
+			origin = "Alice"
+		elif arg == '2':
+			verif = Signature_ElGamal.verif(SHA_1(texte), alpha, prime, s1, s2 , BobPublicKey)
+			origin = "Bob"
+		if verif == True:
+			print("L'empreinte du message et l'empreinte issue du déchiffrement de la signature sont identiques")
+			print("Le message est intègre et " + origin + " en est bien l'auteur\n")
+		else:
+			print("L'intégrite et l'authentification du message n'ont pas pu être vérifiés\n")
+
+
+	# ECHANGE RSA
+	elif arg == '2':
+		public_key, private_key = RSA.init_alice()
+		print("Génération d'un couple de clés \n PUBLIC_KEY:", publickey, '\nPRIVATE_KEY', privatekey)
+		signature = RSA.text_signature(texte, private_key)
+		print("RESULTAT DE LA SIGNATURE PAR RSA AVEC UNE CLE PRIVEE: \n", signature)
+		
+		print("Vérificiation de la signature.\nVoulez vous essayer avec un message modifié ?\n->1<- Non \n->2<- Oui")
+		arg = input(PROMPT)
+		if arg == '1': 
+			RSA.bob_signature_verification(unchecked_message=texte, signature=signature, public_key=public_key)
+		elif arg == '2':
+			print('Nouveau texte : GS15 est une très bonne matière')
+			texte = "GS15 est une très bonne matière"
+			RSA.bob_signature_verification(unchecked_message=texte, signature=signature, public_key=public_key)
+
+
+
+main_menu_sequence()
